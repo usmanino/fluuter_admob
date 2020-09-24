e2535a73d47292ee4a5409d83b52acf49362dad2
usmanino <usmaninobello@gmail.com>
2020-09-24 11:56:41 +0100

    main project

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_admob/firebase_admob.dart';
+
+const String testDevice = 'B4C322FFB67557B1EA5365C0CAF7375D';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    testDevices: testDevice != null ? <String>[testDevice] : null,
+    nonPersonalizedAds: true,
+    keywords: <String>['Game', 'Mario'],
+  );
+
+  BannerAd _bannerAd;
+  InterstitialAd _interstitialAd;
+
+  BannerAd createBannerAd() {
+    return BannerAd(
+        adUnitId: 'ca-app-pub-1958243021759092/2511996625',
+        //Change BannerAd adUnitId with Admob ID
+        size: AdSize.banner,
+        targetingInfo: targetingInfo,
+        listener: (MobileAdEvent event) {
+          print("BannerAd $event");
+        });
+  }
+
+  InterstitialAd createInterstitialAd() {
+    return InterstitialAd(
+        adUnitId: 'ca-app-pub-1958243021759092/2320424934',
+        //Change Interstitial AdUnitId with Admob ID
+        targetingInfo: targetingInfo,
+        listener: (MobileAdEvent event) {
+          print("IntersttialAd $event");
+        });
+  }
+
+  @override
+  void initState() {
+    FirebaseAdMob.instance
+        .initialize(appId: 'ca-app-pub-1958243021759092~2413075694');
+    //Change appId With Admob Id
+    _bannerAd = createBannerAd()
+      ..load()
+      ..show();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd.dispose();
+    _interstitialAd.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Demo App"),
+        ),
+        body: Center(
+            child: RaisedButton(
+          child: Text('Click on Ads'),
+          onPressed: () {
+            createInterstitialAd()
+              ..load()
+              ..show();
+          },
+        )),
+      ),
+    );
+  }
+}
